3b4d355a2c99101ae34bfb97a69fa915deb4dfab
PeacePan <peace.pan@wonderpet.asia>
2021-01-08 11:01:29 +0800

    feat(deploy): rename folder

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/home';
-import './index.scss';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
